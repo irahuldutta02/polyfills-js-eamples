0e0dd094ddd7bcc7b82f36a0633b2b0d6e03e9b9
irahuldutta02 <rdtech2002@gmail.com>
2023-12-27 23:34:15 +0530

    fix: forEach.js

--- forEach.js
+++ forEach.js
@@ -3,12 +3,14 @@
 const arr = [1, 2, 3, 4, 5];
 
 // default forEach function <- just for reference
+/*
 arr.forEach((item, index) => {
   console.log(item, index);
 });
+*/
 
 // custom forEach function
-Array.prototype.customForEach = function(callBack) {
+Array.prototype.customForEach = function (callBack) {
   for (let index = 0; index < this.length; index++) {
     callBack(this[index], index, this);
   }
